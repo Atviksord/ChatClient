e1c3be522197b250f223eaddcbd31a08a2355913
Atviksord <Atviksord@gmail.com>
2024-11-03 22:20:27 +0900

    small update

--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
+
+// struct to keep DB, ENV info etc
+type apiConfig struct {
+	db string
+}
 
 func main() {
 	fmt.Println("Will build a real time chat client with sockets here.")
 
-	d := http.NewServeMux()
-	servr := http.Server{Addr: "0.0.0.0.0", Handler: d}
+	serverIP := "0.0.0.0:8080"
 
-	http.ListenAndServe("0.0.0.0", d)
+	d := http.NewServeMux()
+	servr := http.Server{Addr: serverIP, Handler: d}
+
+	err := servr.ListenAndServe()
+	if err != nil {
+		log.Fatal("Couldnt start server", err)
+	}
 }

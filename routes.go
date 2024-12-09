019a1f2033f240985fd8a0523014073ae125f809
Atviksord <Atviksord@gmail.com>
2024-12-09 17:21:44 +0900

    user creation

--- routes.go
+++ routes.go
@@ -2,10 +2,15 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 	"time"
 
+	"github.com/Atviksord/ChatClient/internal/database"
 	"github.com/gorilla/websocket"
 )
 
@@ -19,12 +24,34 @@ func (cfg *apiConfig) handlerRegistry(mux *http.ServeMux) {
 func (cfg *apiConfig) startHandler(w http.ResponseWriter, r *http.Request) {
 	// auth check
 
+	// if not authed, infinite loop awaiting server commands
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("Enter username: ")
+		username, _ := reader.ReadString('\n')
+		username = strings.TrimSpace(username)
+
+		fmt.Print("Password please:")
+		password, _ := reader.ReadString('\n')
+		password = strings.TrimSpace(password)
+
+		cfg.db.CreateUser(r.Context(),
+			database.CreateUserParams{Username: username,
+				Password:  password,
+				CreatedAt: time.Now().UTC(),
+				UpdatedAt: time.Now().UTC()})
+		break
+
+	}
+	// Will create a cookie to validate login next
+
 }
 
 // Function that establishes the websocket upgrade
 func (cfg *apiConfig) establishConnectionHandler(w http.ResponseWriter, r *http.Request) {
 	upper := websocket.Upgrader{HandshakeTimeout: time.Minute * 10, ReadBufferSize: 0, WriteBufferSize: 0, CheckOrigin: nil}
 	connection, err := upper.Upgrade(w, r, nil)
+
 	if err != nil {
 		log.Printf("WebSocket upgrade failed: %v", err)
 		return
